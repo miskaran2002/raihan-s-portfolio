575cc63bd8891be061eb9f66bdac4086b6662d96
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2025-08-11 13:47:56 -0500

    update

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,6 +2,8 @@ import {  createBrowserRouter,} from "react-router";
 import RootLayout from "../layouts/RootLayout";
 import Home from "../page/Home/Home/Home";
 import About from "../page/About/About";
+import Projects from "../page/About/Projects/Projects";
+import Contact from "../page/Contact/Contact";
  export const router = createBrowserRouter([
     {
         path: "/",
@@ -14,7 +16,17 @@ import About from "../page/About/About";
             {
               path:"/about",
               Component:About
+            },
+            {
+                path: "/projects",
+                Component:Projects
+            },
+            {
+                path:"/contact",
+                Component:Contact
             }
+
+            
         ]
     },
 ]);
